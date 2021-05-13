457c51126c79f12b23562b4f72a494ccc0cacdd3
cmmerritt <christiane.merritt@gmail.com>
2021-05-12 22:35:53 -0700

    yelp route works

--- data/yelpData.js
+++ data/yelpData.js
@@ -1,146 +1,155 @@
-export default [
-  {
-    'id': 'o47W8-SXvcnQbCsedPnbBQ',
-    'alias': 'citizen-burger-bar-charlottesville',
-    'name': 'Citizen Burger Bar',
-    'image_url': 'https://s3-media1.fl.yelpcdn.com/bphoto/FVQ2yaguG7EU0c0QtTrM8w/o.jpg',
-    'is_closed': false,
-    'url': 'https://www.yelp.com/biz/citizen-burger-bar-charlottesville?adjust_creative=_Vy-qm2d5ex1tULRpgPgfQ&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=_Vy-qm2d5ex1tULRpgPgfQ',
-    'review_count': 909,
-    'categories': [
-      {
-        'alias': 'burgers',
-        'title': 'Burgers'
+export default {
+  'businesses': [
+    {
+      'id': 'o47W8-SXvcnQbCsedPnbBQ',
+      'alias': 'citizen-burger-bar-charlottesville',
+      'name': 'Citizen Burger Bar',
+      'image_url': 'https://s3-media1.fl.yelpcdn.com/bphoto/FVQ2yaguG7EU0c0QtTrM8w/o.jpg',
+      'is_closed': false,
+      'url': 'https://www.yelp.com/biz/citizen-burger-bar-charlottesville?adjust_creative=_Vy-qm2d5ex1tULRpgPgfQ&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=_Vy-qm2d5ex1tULRpgPgfQ',
+      'review_count': 909,
+      'categories': [
+        {
+          'alias': 'burgers',
+          'title': 'Burgers'
+        },
+        {
+          'alias': 'bars',
+          'title': 'Bars'
+        },
+        {
+          'alias': 'salad',
+          'title': 'Salad'
+        }
+      ],
+      'rating': 4.0,
+      'coordinates': {
+        'latitude': 38.0304396046825,
+        'longitude': -78.4805095503646
       },
-      {
-        'alias': 'bars',
-        'title': 'Bars'
+      'transactions': [
+        'delivery'
+      ],
+      'price': '$$',
+      'location': {
+        'address1': '212 E Main St',
+        'address2': '',
+        'address3': '',
+        'city': 'Charlottesville',
+        'zip_code': '22902',
+        'country': 'US',
+        'state': 'VA',
+        'display_address': [
+          '212 E Main St',
+          'Charlottesville, VA 22902'
+        ]
       },
-      {
-        'alias': 'salad',
-        'title': 'Salad'
-      }
-    ],
-    'rating': 4.0,
-    'coordinates': {
-      'latitude': 38.0304396046825,
-      'longitude': -78.4805095503646
+      'phone': '+14349799944',
+      'display_phone': '(434) 979-9944',
+      'distance': 341.421639009225
     },
-    'transactions': [
-      'delivery'
-    ],
-    'price': '$$',
-    'location': {
-      'address1': '212 E Main St',
-      'address2': '',
-      'address3': '',
-      'city': 'Charlottesville',
-      'zip_code': '22902',
-      'country': 'US',
-      'state': 'VA',
-      'display_address': [
-        '212 E Main St',
-        'Charlottesville, VA 22902'
-      ]
-    },
-    'phone': '+14349799944',
-    'display_phone': '(434) 979-9944',
-    'distance': 341.421639009225
-  },
-  {
-    'id': '9PA2SuDpygh_DbPPeXlQ8A',
-    'alias': 'ace-biscuit-and-barbecue-charlottesville-3',
-    'name': 'Ace Biscuit & Barbecue',
-    'image_url': 'https://s3-media2.fl.yelpcdn.com/bphoto/qz0ER0bHENosiJhyzgjhRg/o.jpg',
-    'is_closed': false,
-    'url': 'https://www.yelp.com/biz/ace-biscuit-and-barbecue-charlottesville-3?adjust_creative=_Vy-qm2d5ex1tULRpgPgfQ&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=_Vy-qm2d5ex1tULRpgPgfQ',
-    'review_count': 658,
-    'categories': [
-      {
-        'alias': 'bbq',
-        'title': 'Barbeque'
+    {
+      'id': '9PA2SuDpygh_DbPPeXlQ8A',
+      'alias': 'ace-biscuit-and-barbecue-charlottesville-3',
+      'name': 'Ace Biscuit & Barbecue',
+      'image_url': 'https://s3-media2.fl.yelpcdn.com/bphoto/qz0ER0bHENosiJhyzgjhRg/o.jpg',
+      'is_closed': false,
+      'url': 'https://www.yelp.com/biz/ace-biscuit-and-barbecue-charlottesville-3?adjust_creative=_Vy-qm2d5ex1tULRpgPgfQ&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=_Vy-qm2d5ex1tULRpgPgfQ',
+      'review_count': 658,
+      'categories': [
+        {
+          'alias': 'bbq',
+          'title': 'Barbeque'
+        },
+        {
+          'alias': 'breakfast_brunch',
+          'title': 'Breakfast & Brunch'
+        },
+        {
+          'alias': 'soulfood',
+          'title': 'Soul Food'
+        }
+      ],
+      'rating': 4.5,
+      'coordinates': {
+        'latitude': 38.03766433,
+        'longitude': -78.4833315
       },
-      {
-        'alias': 'breakfast_brunch',
-        'title': 'Breakfast & Brunch'
+      'transactions': [
+        'delivery'
+      ],
+      'price': '$$',
+      'location': {
+        'address1': '600 Concord Ave',
+        'address2': '',
+        'address3': '',
+        'city': 'Charlottesville',
+        'zip_code': '22903',
+        'country': 'US',
+        'state': 'VA',
+        'display_address': [
+          '600 Concord Ave',
+          'Charlottesville, VA 22903'
+        ]
       },
-      {
-        'alias': 'soulfood',
-        'title': 'Soul Food'
-      }
-    ],
-    'rating': 4.5,
-    'coordinates': {
-      'latitude': 38.03766433,
-      'longitude': -78.4833315
+      'phone': '+14342021403',
+      'display_phone': '(434) 202-1403',
+      'distance': 1092.0919086694848
     },
-    'transactions': [
-      'delivery'
-    ],
-    'price': '$$',
-    'location': {
-      'address1': '600 Concord Ave',
-      'address2': '',
-      'address3': '',
-      'city': 'Charlottesville',
-      'zip_code': '22903',
-      'country': 'US',
-      'state': 'VA',
-      'display_address': [
-        '600 Concord Ave',
-        'Charlottesville, VA 22903'
-      ]
-    },
-    'phone': '+14342021403',
-    'display_phone': '(434) 202-1403',
-    'distance': 1092.0919086694848
-  },
-  {
-    'id': 'bTCzss9ro-mAhzJKMtULfQ',
-    'alias': 'lampo-charlottesville',
-    'name': 'Lampo',
-    'image_url': 'https://s3-media3.fl.yelpcdn.com/bphoto/puwID0fAmWnKvpri5psb9Q/o.jpg',
-    'is_closed': false,
-    'url': 'https://www.yelp.com/biz/lampo-charlottesville?adjust_creative=_Vy-qm2d5ex1tULRpgPgfQ&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=_Vy-qm2d5ex1tULRpgPgfQ',
-    'review_count': 391,
-    'categories': [
-      {
-        'alias': 'italian',
-        'title': 'Italian'
+    {
+      'id': 'bTCzss9ro-mAhzJKMtULfQ',
+      'alias': 'lampo-charlottesville',
+      'name': 'Lampo',
+      'image_url': 'https://s3-media3.fl.yelpcdn.com/bphoto/puwID0fAmWnKvpri5psb9Q/o.jpg',
+      'is_closed': false,
+      'url': 'https://www.yelp.com/biz/lampo-charlottesville?adjust_creative=_Vy-qm2d5ex1tULRpgPgfQ&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=_Vy-qm2d5ex1tULRpgPgfQ',
+      'review_count': 391,
+      'categories': [
+        {
+          'alias': 'italian',
+          'title': 'Italian'
+        },
+        {
+          'alias': 'pizza',
+          'title': 'Pizza'
+        },
+        {
+          'alias': 'beer_and_wine',
+          'title': 'Beer, Wine & Spirits'
+        }
+      ],
+      'rating': 4.5,
+      'coordinates': {
+        'latitude': 38.0273899,
+        'longitude': -78.47684
       },
-      {
-        'alias': 'pizza',
-        'title': 'Pizza'
+      'transactions': [
+        'delivery'
+      ],
+      'price': '$$',
+      'location': {
+        'address1': '205 Monticello Rd',
+        'address2': '',
+        'address3': '',
+        'city': 'Charlottesville',
+        'zip_code': '22902',
+        'country': 'US',
+        'state': 'VA',
+        'display_address': [
+          '205 Monticello Rd',
+          'Charlottesville, VA 22902'
+        ]
       },
-      {
-        'alias': 'beer_and_wine',
-        'title': 'Beer, Wine & Spirits'
-      }
-    ],
-    'rating': 4.5,
-    'coordinates': {
-      'latitude': 38.0273899,
-      'longitude': -78.47684
-    },
-    'transactions': [
-      'delivery'
-    ],
-    'price': '$$',
-    'location': {
-      'address1': '205 Monticello Rd',
-      'address2': '',
-      'address3': '',
-      'city': 'Charlottesville',
-      'zip_code': '22902',
-      'country': 'US',
-      'state': 'VA',
-      'display_address': [
-        '205 Monticello Rd',
-        'Charlottesville, VA 22902'
-      ]
-    },
-    'phone': '+14342443226',
-    'display_phone': '(434) 244-3226',
-    'distance': 218.45934125894814
+      'phone': '+14342443226',
+      'display_phone': '(434) 244-3226',
+      'distance': 218.45934125894814
+    }
+  ],
+  'total': 381,
+  'region': {
+    'center': {
+      'longitude': -78.4766781,
+      'latitude': 38.029306
+    }
   }
-];
+};
