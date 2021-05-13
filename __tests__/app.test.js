c9c2581369023fc9a392324bcf50c4e3135dee6e
cmmerritt <christiane.merritt@gmail.com>
2021-05-12 21:47:41 -0700

    yelp munge function works

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,7 +1,8 @@
 import locationIqData from '../data/locationIqData';
-import { formatLocation } from '../lib/utils';
+import yelpData from '../data/yelpData';
+import { formatLocation, formatYelp } from '../lib/utils';
 
-describe('API Location Data Munging', () => {
+describe('API location data munging', () => {
   const expectedLocations = 
     { 'formatted_query': 'Charlottesville, Virginia, USA', 
       'latitude': '38.029306',
@@ -11,5 +12,36 @@ describe('API Location Data Munging', () => {
   it('munges location data', async () => {
     const output = formatLocation(locationIqData);
     expect(output).toEqual(expectedLocations);
+  });
+});
+
+describe('API yelp data munging', () => {
+  const expectedYelps = [
+    {
+      'name': 'Citizen Burger Bar',
+      'image_url': 'https://s3-media1.fl.yelpcdn.com/bphoto/FVQ2yaguG7EU0c0QtTrM8w/o.jpg',
+      'price': '$$',
+      'rating': 4.0,
+      'url': 'https://www.yelp.com/biz/citizen-burger-bar-charlottesville?adjust_creative=_Vy-qm2d5ex1tULRpgPgfQ&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=_Vy-qm2d5ex1tULRpgPgfQ'
+    },
+    {
+      'name': 'Ace Biscuit & Barbecue',
+      'image_url': 'https://s3-media2.fl.yelpcdn.com/bphoto/qz0ER0bHENosiJhyzgjhRg/o.jpg',
+      'price': '$$',
+      'rating': 4.5,
+      'url': 'https://www.yelp.com/biz/ace-biscuit-and-barbecue-charlottesville-3?adjust_creative=_Vy-qm2d5ex1tULRpgPgfQ&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=_Vy-qm2d5ex1tULRpgPgfQ'
+    },
+    {
+      'name': 'Lampo',
+      'image_url': 'https://s3-media3.fl.yelpcdn.com/bphoto/puwID0fAmWnKvpri5psb9Q/o.jpg',
+      'price': '$$',
+      'rating': 4.5,
+      'url': 'https://www.yelp.com/biz/lampo-charlottesville?adjust_creative=_Vy-qm2d5ex1tULRpgPgfQ&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=_Vy-qm2d5ex1tULRpgPgfQ'
+    }
+  ];
+
+  it('munges yelp data', async () => {
+    const output = formatYelp(yelpData);
+    expect(output).toEqual(expectedYelps);
   });
 });
--- data/yelpData.js
+++ data/yelpData.js
@@ -1,53 +1,146 @@
-export default {
-  'businesses': [
-    {
-      'id': 'OfQhFTk5r7uZ5wOrXMUQ1g',
-      'alias': 'hunt-country-market-charlottesville',
-      'name': 'Hunt Country Market',
-      'image_url': 'https://s3-media3.fl.yelpcdn.com/bphoto/b2cILOCgGQyctd8wtT6GrQ/o.jpg',
-      'is_closed': false,
-      'url': 'https://www.yelp.com/biz/hunt-country-market-charlottesville?adjust_creative=_Vy-qm2d5ex1tULRpgPgfQ&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=_Vy-qm2d5ex1tULRpgPgfQ',
-      'review_count': 14,
-      'categories': [
-        {
-          'alias': 'delis',
-          'title': 'Delis'
-        },
-        {
-          'alias': 'grocery',
-          'title': 'Grocery'
-        }
-      ],
-      'rating': 4.0,
-      'coordinates': {
-        'latitude': 38.1031645,
-        'longitude': -78.5554366
+export default [
+  {
+    'id': 'o47W8-SXvcnQbCsedPnbBQ',
+    'alias': 'citizen-burger-bar-charlottesville',
+    'name': 'Citizen Burger Bar',
+    'image_url': 'https://s3-media1.fl.yelpcdn.com/bphoto/FVQ2yaguG7EU0c0QtTrM8w/o.jpg',
+    'is_closed': false,
+    'url': 'https://www.yelp.com/biz/citizen-burger-bar-charlottesville?adjust_creative=_Vy-qm2d5ex1tULRpgPgfQ&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=_Vy-qm2d5ex1tULRpgPgfQ',
+    'review_count': 909,
+    'categories': [
+      {
+        'alias': 'burgers',
+        'title': 'Burgers'
       },
-      'transactions': [],
-      'price': '$',
-      'location': {
-        'address1': '2048 Garth Rd',
-        'address2': '',
-        'address3': '',
-        'city': 'Charlottesville',
-        'zip_code': '22901',
-        'country': 'US',
-        'state': 'VA',
-        'display_address': [
-          '2048 Garth Rd',
-          'Charlottesville, VA 22901'
-        ]
+      {
+        'alias': 'bars',
+        'title': 'Bars'
       },
-      'phone': '+14342961648',
-      'display_phone': '(434) 296-1648',
-      'distance': 2452.996792346069
-    }
-  ],
-  'total': 1,
-  'region': {
-    'center': {
-      'longitude': -78.543,
-      'latitude': 38.123
-    }
+      {
+        'alias': 'salad',
+        'title': 'Salad'
+      }
+    ],
+    'rating': 4.0,
+    'coordinates': {
+      'latitude': 38.0304396046825,
+      'longitude': -78.4805095503646
+    },
+    'transactions': [
+      'delivery'
+    ],
+    'price': '$$',
+    'location': {
+      'address1': '212 E Main St',
+      'address2': '',
+      'address3': '',
+      'city': 'Charlottesville',
+      'zip_code': '22902',
+      'country': 'US',
+      'state': 'VA',
+      'display_address': [
+        '212 E Main St',
+        'Charlottesville, VA 22902'
+      ]
+    },
+    'phone': '+14349799944',
+    'display_phone': '(434) 979-9944',
+    'distance': 341.421639009225
+  },
+  {
+    'id': '9PA2SuDpygh_DbPPeXlQ8A',
+    'alias': 'ace-biscuit-and-barbecue-charlottesville-3',
+    'name': 'Ace Biscuit & Barbecue',
+    'image_url': 'https://s3-media2.fl.yelpcdn.com/bphoto/qz0ER0bHENosiJhyzgjhRg/o.jpg',
+    'is_closed': false,
+    'url': 'https://www.yelp.com/biz/ace-biscuit-and-barbecue-charlottesville-3?adjust_creative=_Vy-qm2d5ex1tULRpgPgfQ&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=_Vy-qm2d5ex1tULRpgPgfQ',
+    'review_count': 658,
+    'categories': [
+      {
+        'alias': 'bbq',
+        'title': 'Barbeque'
+      },
+      {
+        'alias': 'breakfast_brunch',
+        'title': 'Breakfast & Brunch'
+      },
+      {
+        'alias': 'soulfood',
+        'title': 'Soul Food'
+      }
+    ],
+    'rating': 4.5,
+    'coordinates': {
+      'latitude': 38.03766433,
+      'longitude': -78.4833315
+    },
+    'transactions': [
+      'delivery'
+    ],
+    'price': '$$',
+    'location': {
+      'address1': '600 Concord Ave',
+      'address2': '',
+      'address3': '',
+      'city': 'Charlottesville',
+      'zip_code': '22903',
+      'country': 'US',
+      'state': 'VA',
+      'display_address': [
+        '600 Concord Ave',
+        'Charlottesville, VA 22903'
+      ]
+    },
+    'phone': '+14342021403',
+    'display_phone': '(434) 202-1403',
+    'distance': 1092.0919086694848
+  },
+  {
+    'id': 'bTCzss9ro-mAhzJKMtULfQ',
+    'alias': 'lampo-charlottesville',
+    'name': 'Lampo',
+    'image_url': 'https://s3-media3.fl.yelpcdn.com/bphoto/puwID0fAmWnKvpri5psb9Q/o.jpg',
+    'is_closed': false,
+    'url': 'https://www.yelp.com/biz/lampo-charlottesville?adjust_creative=_Vy-qm2d5ex1tULRpgPgfQ&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=_Vy-qm2d5ex1tULRpgPgfQ',
+    'review_count': 391,
+    'categories': [
+      {
+        'alias': 'italian',
+        'title': 'Italian'
+      },
+      {
+        'alias': 'pizza',
+        'title': 'Pizza'
+      },
+      {
+        'alias': 'beer_and_wine',
+        'title': 'Beer, Wine & Spirits'
+      }
+    ],
+    'rating': 4.5,
+    'coordinates': {
+      'latitude': 38.0273899,
+      'longitude': -78.47684
+    },
+    'transactions': [
+      'delivery'
+    ],
+    'price': '$$',
+    'location': {
+      'address1': '205 Monticello Rd',
+      'address2': '',
+      'address3': '',
+      'city': 'Charlottesville',
+      'zip_code': '22902',
+      'country': 'US',
+      'state': 'VA',
+      'display_address': [
+        '205 Monticello Rd',
+        'Charlottesville, VA 22902'
+      ]
+    },
+    'phone': '+14342443226',
+    'display_phone': '(434) 244-3226',
+    'distance': 218.45934125894814
   }
-};
+];
